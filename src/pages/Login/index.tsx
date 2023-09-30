c33ae3adbb714e2b5f645251f1b37704e207f863
Vitor Martins <vitomartinsalmeida@gmail.com>
2023-09-30 17:28:29 -0300

    realizando chamada para a API e retornando pagina

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import './index.css';
+import { useNavigate } from 'react-router-dom';
 import { ChangeType } from '../../types';
+import { createUser } from '../../services/userAPI';
 
 const INITIAL_FORM = {
   name: '',
 };
 
 function Login() {
+  const [isLoading, setIsLoading] = useState(false);
   const [isDisabled, setIsDisabled] = useState(true);
   const [formInfo, setFormInfo] = useState(INITIAL_FORM);
+  const navigate = useNavigate();
 
   const handleChange = (event: ChangeType) => {
     const { name, value } = event.target;
@@ -19,23 +23,40 @@ function Login() {
     setIsDisabled(value.length < 3);
   };
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+    const response = await createUser(formInfo);
+    if (response === 'OK') {
+      setIsLoading(false);
+      (navigate('/search'));
+    }
+  };
+
   return (
-    <div className="form-container">
-      <form className="login-form">
-        <label htmlFor="name">
-          <input
-            data-testid="login-name-input"
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Nome"
-            value={ formInfo.name }
-            onChange={ handleChange }
-          />
-        </label>
-        <button disabled={ isDisabled } data-testid="login-submit-button">Entrar</button>
-      </form>
-    </div>
+    isLoading ? (<h1>Carregando...</h1>) : (
+      <div className="form-container">
+        <form onSubmit={ handleSubmit } className="login-form">
+          <label htmlFor="name">
+            <input
+              data-testid="login-name-input"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Nome"
+              value={ formInfo.name }
+              onChange={ handleChange }
+            />
+          </label>
+          <button
+            disabled={ isDisabled }
+            data-testid="login-submit-button"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    )
   );
 }
 
---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,5 @@
+function Search() {
+  return (<h1>Hello World</h1>);
+}
+
+export default Search;
