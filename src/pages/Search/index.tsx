f99a88aa8fc57b54d3818640b5de4362a225ca2a
Vitor Martins <vitomartinsalmeida@gmail.com>
2023-09-30 17:52:27 -0300

    criando forumlário na rota Search

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,5 +1,30 @@
+import { useState } from 'react';
+import { ChangeType } from '../../types';
+
 function Search() {
-  return (<h1>Hello World</h1>);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [artist, setArtist] = useState('');
+
+  const handleChange = (event: ChangeType) => {
+    const { value } = event.target;
+    setArtist(value);
+    setIsDisabled(value.length < 2);
+  };
+
+  return (
+    <form>
+      <input
+        data-testid="search-artist-input"
+        type="text"
+        name="artist"
+        id="artist"
+        placeholder="Nome do Artista"
+        value={ artist }
+        onChange={ handleChange }
+      />
+      <button disabled={ isDisabled } data-testid="search-artist-button">Procurar</button>
+    </form>
+  );
 }
 
 export default Search;
