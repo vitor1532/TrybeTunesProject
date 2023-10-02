d4f9a58edb97df8c126a1bb73395e984378cd3aa
Vitor Martins <vitomartinsalmeida@gmail.com>
2023-10-02 19:46:38 -0300

    chamando a função getUser no componente Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,46 @@
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import Loading from './Loading';
+import { UserType } from '../types';
+import { getUser } from '../services/userAPI';
 
 function Header() {
-  return (
-    <header data-testid="header-component">
-      <div className="logo-container">
-        <div className="user-container">
-          <p data-testid="header-user-name">usuario</p>
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState<UserType | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        setIsLoading(true);
+        const userData = await getUser();
+        setUser(userData);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  if (isLoading) return (<Loading />);
+
+  if (user !== null) {
+    return (
+      <header data-testid="header-component">
+        <div className="logo-container">
+          <div className="user-container">
+            <p data-testid="header-user-name">{user.name}</p>
+          </div>
         </div>
-      </div>
-      <nav>
-        <NavLink data-testid="link-to-search" to="/search">Search</NavLink>
-        <NavLink data-testid="link-to-favorites" to="/favorites">Favorites</NavLink>
-        <NavLink data-testid="link-to-profile" to="/profile">Profile</NavLink>
-      </nav>
-    </header>
-  );
+        <nav>
+          <NavLink data-testid="link-to-search" to="/search">Search</NavLink>
+          <NavLink data-testid="link-to-favorites" to="/favorites">Favorites</NavLink>
+          <NavLink data-testid="link-to-profile" to="/profile">Profile</NavLink>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default Header;
