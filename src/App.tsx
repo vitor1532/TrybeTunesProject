5a3c428a9f7c1b34f63e73a229e110f21cb20abf
Vitor Martins <vitomartinsalmeida@gmail.com>
2023-10-02 19:57:07 -0300

    criando e renderizando componente Layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Login /> } />
-      <Route path="/search" element={ <Search /> } />
-      <Route path="/album/:id" element={ <Album /> } />
-      {/* <Route path="/favorites" element={ <Favorites /> } /> */}
-      {/* <Route path="/profile" element={ <Profile /> } /> */}
-      {/* <Route path="/profile/edit" element={ <ProfileEdit /> } /> */}
+      <Route index element={ <Login /> } />
+      <Route path="/" element={ <Layout /> }>
+        <Route path="/search" element={ <Search /> } />
+        <Route path="/album/:id" element={ <Album /> } />
+        {/* <Route path="/favorites" element={ <Favorites /> } /> */}
+        {/* <Route path="/profile" element={ <Profile /> } /> */}
+        {/* <Route path="/profile/edit" element={ <ProfileEdit /> } /> */}
+      </Route>
       {/* <Route path="/*" element={ <NotFound /> } /> */}
     </Routes>
   );
