74c0deae844649afee704ce64b4f8af4fd546616
vitor1532 <vitomartinsalmeida@gmail.com>
2023-10-05 13:20:44 -0300

    estilizando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import Loading from './Loading';
 import { UserType } from '../types';
 import { getUser } from '../services/userAPI';
+import '../styles/Header.css';
 
 function Header() {
   const [isLoading, setIsLoading] = useState(false);
@@ -28,16 +30,37 @@ function Header() {
   if (user !== null) {
     return (
       <header data-testid="header-component">
-        <div className="logo-container">
-          <div className="user-container">
-            <p data-testid="header-user-name">{user.name}</p>
-          </div>
-        </div>
-        <nav>
-          <NavLink data-testid="link-to-search" to="/search">Search</NavLink>
-          <NavLink data-testid="link-to-favorites" to="/favorites">Favorites</NavLink>
-          <NavLink data-testid="link-to-profile" to="/profile">Profile</NavLink>
-        </nav>
+
+        <Navbar bg="primary" data-bs-theme="dark">
+          <Container>
+            <Nav className="me-auto">
+              <NavLink
+                className="nav-link"
+                data-testid="link-to-search"
+                to="/search"
+              >
+                Search
+              </NavLink>
+              <NavLink
+                className="nav-link"
+                data-testid="link-to-favorites"
+                to="/favorites"
+              >
+                Favorites
+              </NavLink>
+              <NavLink
+                className="nav-link"
+                data-testid="link-to-profile"
+                to="/profile"
+              >
+                Profile
+              </NavLink>
+            </Nav>
+            <Navbar.Brand href="/profile">
+              <p data-testid="header-user-name">{user.name}</p>
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
       </header>
     );
   }
