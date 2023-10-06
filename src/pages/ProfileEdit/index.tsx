189ea770701272feec9d145446863b680666ed35
Vitor Martins <vitomartinsalmeida@gmail.com>
2023-10-06 20:26:46 -0300

    estilizando ProfileEdit

--- src/pages/ProfileEdit/index.tsx
+++ src/pages/ProfileEdit/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 import Loading from '../../components/Loading';
 import { ChangeType, UserType } from '../../types';
 import { getUser, updateUser } from '../../services/userAPI';
@@ -67,61 +68,80 @@ function ProfileEdit() {
   if (isLoading) return (<Loading />);
 
   return (
-    <form
-      onSubmit={ handleSubmit }
-    >
-      <label htmlFor="image">
-        <input
-          type="text"
-          name="image"
-          id="image"
-          onChange={ handleChange }
-          value={ image }
-          data-testid="edit-input-image"
-        />
-        {image
+    <div className="perfil-form-container">
+      <h3 className="title-edit">Edit you profile</h3>
+      <Form
+        className="perfil-form"
+        onSubmit={ handleSubmit }
+      >
+        <Row>
+          <Form.Group as={ Col }>
+            <Form.Label>Image</Form.Label>
+            <Form.Control
+              type="text"
+              name="image"
+              id="image"
+              value={ image }
+              data-testid="edit-input-image"
+              onChange={ (event) => handleChange(event as ChangeType) }
+            />
+          </Form.Group>
+          <Form.Group as={ Col }>
+            {image
         && <img className="placeholder-image" src={ image } alt="foto-perfil" />}
+          </Form.Group>
+        </Row>
 
-      </label>
-      <label htmlFor="name">
-        <h4>Name</h4>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Feel free to use your social name"
-          value={ name }
-          onChange={ handleChange }
-          data-testid="edit-input-name"
-        />
-      </label>
-      <label htmlFor="email">
-        <h4>E-mail</h4>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Choose an email that you check daily"
-          value={ email }
-          onChange={ handleChange }
-          data-testid="edit-input-email"
-        />
-      </label>
-      <label htmlFor="description">
-        <h4>Description</h4>
-        <textarea
-          name="description"
-          id="description"
-          placeholder="About me"
-          cols={ 30 }
-          rows={ 10 }
-          value={ description }
-          onChange={ handleChange }
-          data-testid="edit-input-description"
-        />
-      </label>
-      <button data-testid="edit-button-save" disabled={ !isFormValid() }>Save</button>
-    </form>
+        <Form.Group>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Feel free to use your social name"
+            value={ name }
+            data-testid="edit-input-name"
+            onChange={ (event) => handleChange(event as ChangeType) }
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>E-mail</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Choose an email that you check daily"
+            value={ email }
+            onChange={ (event) => handleChange(event as ChangeType) }
+            data-testid="edit-input-email"
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            name="description"
+            id="description"
+            placeholder="About me"
+            cols={ 30 }
+            rows={ 10 }
+            size="sm"
+            value={ description }
+            onChange={ (event) => handleChange(event as ChangeType) }
+            data-testid="edit-input-description"
+          />
+        </Form.Group>
+        <Button
+          className="profile-btn"
+          type="submit"
+          variant="success"
+          data-testid="edit-button-save"
+          disabled={ !isFormValid() }
+        >
+          Save
+        </Button>
+      </Form>
+    </div>
   );
 }
 
